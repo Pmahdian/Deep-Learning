b348ccba8fd4e15aa805a8e06fef436e9b4c5b65
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-15 21:04:25 +0330

    importing

---
+++ Fashion-mnist/model.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
