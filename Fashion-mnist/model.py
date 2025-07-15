e1a4d11a2062cd10cc3f45940b329d5baabe1a07
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-15 21:06:09 +0330

    Compile Model

--- Fashion-mnist/model.py
+++ Fashion-mnist/model.py
@@ -6,3 +6,37 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 import numpy as np
+import ssl
+import certifi
+
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+fmnist_data = keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fmnist_data.load_data()
+
+x_train, x_test = x_train/255.0, x_test/255.0
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(75, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(75, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+
+weights, bias = model.layers[1].get_weights()
+
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="sgd",
+              metrics=["accuracy"])
