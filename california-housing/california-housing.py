19cce4f1bc66efe3e71f566f2d57e380c2fc2fa6
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-22 02:29:57 +0330

    fetch_california_housing

--- california-housing/california-housing.py
+++ california-housing/california-housing.py
@@ -7,3 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler  
 import pandas as pd
 import seaborn as sns
+
+
+california_housing = fetch_california_housing(as_frame=True)
